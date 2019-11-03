cf025f752c56987685e4cfd69206e703da6eecdf
shenjian <sjshenjian@outlook.com>
2019-11-03 12:13:16 +0800

    add eureka-client demo

--- eureka-client/src/main/java/online/shenjian/eureka/client/EurekaClientApplication.java
+++ eureka-client/src/main/java/online/shenjian/eureka/client/EurekaClientApplication.java
@@ -1,19 +1,19 @@
-package online.shenjian.eureka.server;
+package online.shenjian.eureka.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Jian Shen
  * @version V1.0.0
  * @date 2019/11/3
  */
-@EnableEurekaServer
+@EnableEurekaClient
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(EurekaClientApplication.class, args);
     }
 }
---
+++ eureka-server/src/main/java/online/shenjian/eureka/client/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package online.shenjian.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Jian Shen
+ * @version V1.0.0
+ * @date 2019/11/3
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
